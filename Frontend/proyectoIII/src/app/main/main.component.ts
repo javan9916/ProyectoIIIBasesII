b37774135b3cc11904470b396e4abf5e32ac2312
javan9916 <jantono9916@gmail.com>
2019-11-19 23:01:14 -0600

    avance2

--- Frontend/proyectoIII/src/app/main/main.component.ts
+++ Frontend/proyectoIII/src/app/main/main.component.ts
@@ -3,8 +3,13 @@ import { MatDialog, MatSort, MatDialogConfig, MatTable, MatTableDataSource } fro
 import { ActivatedRoute } from '@angular/router';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { Curso } from '../models/curso';
+import { Mensaje } from '../models/mensaje';
 
 var cursos: Curso[] = [];
+var mensajes: Mensaje[] = [ {codigo: 1, emisor: 'Jazmine', receptor: 'Javier', mensaje: 'Hola, ¿cómo está?'},
+                            {codigo: 2, emisor: 'Javier', receptor: 'Jazmine', mensaje: 'Súper bien, ¡pasé algoritmos!'},
+                            {codigo: 3, emisor: 'Jazmine', receptor: 'Javier', mensaje: '¡Qué bueno, me alegro!'},
+                            {codigo: 4, emisor: 'Javier', receptor: 'Jazmine', mensaje: ':)'}];
 
 @Component({
   selector: 'app-main',
@@ -18,6 +23,8 @@ export class MainComponent implements OnInit {
   lotsData: any;
   messageForm: FormGroup;
   username: '';
+  msjs = mensajes;
+  blank = ' ';
 
   constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) { }
 
---
+++ Frontend/proyectoIII/src/app/models/mensaje.ts
@@ -0,0 +1,6 @@
+export interface Mensaje {
+    codigo: number,
+    emisor: string,
+    receptor: string,
+    mensaje: string,
+}
